82996df6244d799d98d5b9f5b38dcbfbdbc0e466
manoj2508 <manoj.kumar@housing.com>
2016-05-23 23:28:22 +0530

    setup account

---
+++ app/src/main/java/com/example/manoj/roposoapp/FeedActivity.java
@@ -0,0 +1,13 @@
+package com.example.manoj.roposoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
